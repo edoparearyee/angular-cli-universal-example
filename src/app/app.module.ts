4e07c58913f51c3bf112ef16079187be451b9adb
Edward Opare-Aryee <ed@thisissoon.com>
2017-09-25 23:47:22 +0100

    build server bundle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'cli-universal'}),
     BrowserAnimationsModule,
     AppRoutingModule
   ],
