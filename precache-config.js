27ba5cbd9b869d92e2d972d450a591b818133bac
Edward Opare-Aryee <ed@thisissoon.com>
2017-10-14 20:09:39 +0100

    cache api request in service worker

--- precache-config.js
+++ precache-config.js
@@ -1,21 +1,24 @@
 var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+var argv = require('yargs').argv;
+
+var root = argv.root || 'dist/client';
 
 module.exports = {
   navigateFallback: '/index.html',
   navigateFallbackWhitelist: [/^(?!\/__)/], // <-- necessary for Firebase OAuth
-  stripPrefix: 'dist/client',
-  root: 'dist/client/',
+  stripPrefix: root,
+  root: root + '/',
   plugins: [
     new SWPrecacheWebpackPlugin({
       cacheId: 'cli-universal',
       filename: 'service-worker.js',
       staticFileGlobs: [
-        'dist/client/index.html',
-        'dist/client/**.js',
-        'dist/client/**.css'
+        root + '/index.html',
+        root + '/**.js',
+        root + '/**.css'
       ],
-      stripPrefix: 'dist/client/assets/',
+      stripPrefix: root + '/assets/',
       mergeStaticsConfig: true // if you don't set this to true, you won't see any webpack-emitted assets in your serviceworker config
-    }),
+    })
   ]
 };
--- src/app/about/about-resolve.service.ts
+++ src/app/about/about-resolve.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, Inject } from '@angular/core';
 import { Resolve } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
 import { HttpTransferService } from '@ngx-universal/state-transfer';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
 
 import { environment } from '../../environments/environment';
 import { Post } from '../shared/post.model';
@@ -12,6 +14,7 @@ export class PostResolver implements Resolve<Post[]> {
   constructor(private http: HttpTransferService) { }
 
   public resolve(): Observable<Post[]> {
-    return this.http.get(environment.apiUrl);
+    return this.http.get(environment.apiUrl)
+      .catch(err => Observable.of([]));
   }
 }
